a4be308e85903665d963ebac50137231ba1ad779
Brady Lewis <59935961+ARagorn500@users.noreply.github.com>
2020-03-08 20:29:33 -0500

    Update

--- CIntegerSequences/main.c
+++ CIntegerSequences/main.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void CalculateTotal(int, int, int*);
+void CalculateTotal(int num1, int num2, int* total)
+    {
+        for (int x = num1; x<= num2; x++)
+        {
+            *total += x;
+        }
+    }
 
 int main()
 {
     int num1;
     int num2;
-    int total;
+    int total = 0;
+    int dif;
 
     printf("what is your starting integer?\n");
     scanf("%i", &num1);
@@ -15,15 +22,7 @@ int main()
     printf("What is your ending integer?\n");
     scanf("%i", &num2);
 
-    total = 0;
-
-
-
-
-    for (int x = num1; x<= num2; x++)
-    {
-        total += x;
-    }
+    CalculateTotal(num1, num2, &total);
 
     printf("The sum of the integers from %i to %i = %i", num1, num2, total);
     return 0;
